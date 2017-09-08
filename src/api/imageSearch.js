5c6409a247f25efd10cd18ab5593dcb4ef2db3e7
Julio Olivera <julio.olvr@gmail.com>
2017-09-08 13:02:51 -0300

    Now deployment

--- src/api/imageSearch.js
+++ src/api/imageSearch.js
@@ -1,7 +1,7 @@
 export default {
   // TODO: Get API from conf
   search(query) {
-    return fetch(`http://localhost:8000/search?q=${query}`).then(response =>
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}/search?q=${query}`).then(response =>
       response.json()
     );
   }
