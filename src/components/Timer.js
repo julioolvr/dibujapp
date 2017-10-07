df9c7ec5138c7ee7ac2c31a9e9bdbb060ffe480e
Julio Olivera <julio.olvr@gmail.com>
2017-10-06 20:59:36 -0700

    Some timer styles

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,26 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import g from "glamorous";
 
 import { startTimer, stopTimer } from "../ducks/timer";
+
+const TimerContainer = g.div({
+  textAlign: "center"
+});
+
+const TimerValue = g.div({
+  fontSize: "2.3em",
+  color: "#e24e42"
+});
+
+const TimerButton = g.button({
+  backgroundColor: "#eee",
+  border: "1px solid #008f95",
+  color: "#008f95",
+  fontSize: "0.6em",
+  padding: "1em 2em",
+  outline: "none",
+  cursor: "pointer",
+  width: "10em"
+});
 
 function formatSeconds(seconds) {
   const minutes = Math.floor(seconds / 60);
   const secondsLeft = seconds % 60;
 
-  return `${minutes}:${secondsLeft.toString().padStart(2, "0")}`;
+  return `${minutes}${secondsLeft % 2 === 0
+    ? ":"
+    : " "}${secondsLeft.toString().padStart(2, "0")}`;
 }
 
 function Timer({ elapsedSeconds, limit, running, onStart, onStop }) {
   return (
-    <div>
-      {formatSeconds(limit - elapsedSeconds)}
+    <TimerContainer>
+      <TimerValue>{formatSeconds(limit - elapsedSeconds)}</TimerValue>
       {running ? (
-        <button onClick={onStop}>Stop</button>
+        <TimerButton onClick={onStop}>Stop</TimerButton>
       ) : (
-        <button onClick={onStart}>Start</button>
+        <TimerButton onClick={onStart}>Start</TimerButton>
       )}
-    </div>
+    </TimerContainer>
   );
 }
 
